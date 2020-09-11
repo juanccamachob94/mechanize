54c3301b3a34787a12a7a1667a5c96ee30d5159c
juanccamachob94 <42450812+juanccamachob94@users.noreply.github.com>
2020-09-11 01:58:11 -0500

    fix

--- login_and_protected_request.rb
+++ login_and_protected_request.rb
@@ -1,17 +1,17 @@
 BASE_URL = '*******'
 LOGIN_PATH = '/login'
-DAIRY_SALES_PATH = '/send'
+SEND_PATH = '/send'
 
 LOGIN_URL = BASE_URL + LOGIN_PATH
-DAIRY_SALES_URL = BASE_URL + DAIRY_SALES_PATH
+SEND_URL = BASE_URL + SEND__PATH
 
-DAIRY_SALES_PARAMS = {
+SEND_PARAMS = {
   'x' => {
     'date' => '2020-09-10'
   }
 }
 
-DAIRY_SALES_HEADER = {'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8' }
+SEND_HEADER = {'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8' }
 
 mechanize = Mechanize.new { |a| a.ssl_version, a.verify_mode = 'TLSv1',OpenSSL::SSL::VERIFY_NONE }
 
@@ -24,4 +24,4 @@ end
 
 build_login_form(mechanize.get(LOGIN_URL)).submit
 # #<Mechanize::Page{url#<URI::HTTPS https://...>}{meta_refresh}{title nil}{iframes}{frames}{links}{forms}> - this appears to be empty but it is not
-mechanize.post(DAIRY_SALES_URL, DAIRY_SALES_PARAMS, DAIRY_SALES_HEADER).body
+mechanize.post(SEND_URL, SEND_PARAMS, SEND_HEADER).body
